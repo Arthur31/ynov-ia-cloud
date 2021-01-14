d2ab1346f91b50c2e595e1a12af3cb4ad4797469
Arthur Marty <arthurmarty@gmail.com>
2021-01-14 10:52:41 +0100

    first page

--- djangoherokuapp/djangoherokuapp/urls.py
+++ djangoherokuapp/djangoherokuapp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from herokuapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hello_world, name="hello_world"),
+    # path('', views.home, name='home')
 ]
